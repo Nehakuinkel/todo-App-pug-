ab7abe3544cbb6640ebdd276bc3c02be05b0d9ab
neha sharma <sharma.neha.ns729@gmail.com>
2022-02-10 10:55:38 +0545

    second commit

--- controller/index.js
+++ controller/index.js
@@ -25,22 +25,26 @@ throw err;
 }
 }
 exports.editPost = (req,res,next)=>{
-    let id = req.params._id;
+    console.log(req.params)
     List.findById(req.params._id, (err, data)=>{
         if(err){throw err;}
-        if(data){return res.render('editPost',{id,data}) }
+        if(data){return res.render('editPost', {
+             id : req.params._id,
+             data
+        }) }
     })
-}
+} 
     
 
 exports.updatePost = (req,res,next)=>{
+   console.log(req.body.editText)
 List.findByIdAndUpdate(req.body.id, 
     {description:req.body.editText}, function(err, data) {
         if(err){
             console.log(err);
         }
         if(data){
-            //console.log("Data updated!");
+            console.log("Data updated!");
             res.redirect('/');
         }
     });   
