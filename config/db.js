9f53e4114f77ee9652eb4f6ea833913baef68d2d
Neha Sharma <sharma.neha.ns729@gmail.com>
2022-11-30 17:19:25 +0545

    added db info

--- config/db.js
+++ config/db.js
@@ -2,11 +2,8 @@
    
 //Import the mongoose module
 var mongoose = require('mongoose');
-var dotenv =  require('dotenv');
-dotenv.config({path:'./config/config.env'})
 
-const mongoDB = process.env.MONGODB_URL;
-mongoose.connect(mongoDB, {useNewUrlParser: true});
+mongoose.connect("mongodb://127.0.0.1:27017", {useNewUrlParser: true});
 var conn = mongoose.connection;
 conn.on('connected', function() {
     console.log('database is connected successfully');
